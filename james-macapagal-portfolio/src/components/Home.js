c8212120a57b9ea5264daf7733f61711ecedea6d
James Macapagal <j.macapagal@gmail.com>
2022-09-10 16:43:53 -0500

    hit mvp

--- james-macapagal-portfolio/src/components/Home.js
+++ james-macapagal-portfolio/src/components/Home.js
@@ -1,12 +1,15 @@
 import ProjectCarousel from "./ProjectCarousel";
+import Projects from "./Projects";
 
 function Home(){
     return (
         <div class="container-xxl">
-            <h1 class="display-1">i am...</h1>
-            <h1 class="display-2">James Macapagal</h1>
+            <h1 class="display-6">my name is </h1>
+            <h1 class="display-2">james macapagal</h1>
+            <h1 class="display-6">and this is my work</h1>
             <ProjectCarousel />
         </div>
+
     )
 }
 
