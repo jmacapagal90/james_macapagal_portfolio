20a49609396fa6fb392eaccd1830cc4e80b33c0e
James Macapagal <j.macapagal@gmail.com>
2022-09-12 12:37:34 -0500

    styling

--- docs/src/components/About.js
+++ docs/src/components/About.js
@@ -8,15 +8,17 @@ function About(){
                 <img src="https://i.imgur.com/T3vXh7N.jpg" alt="profile-pic" class="w-25 rounded-4"/>
             </div>
             <br></br>
-            <p class="lead">I’m a former Business Analyst with 5+ years working within Product Delivery teams and delivering on project deliverables along Agile methodology.
+            <p class="lead">I’m a former Business Analyst with 5+ years working within Product Delivery teams and delivering on project deliverables along Agile methodology. After a fruitful career as a Business Analyst, I decided to transition toward a career as a Software Engineer.</p>
+            <p class="lead"></p>
+            <p class="lead">
 
-After a fruitful career as a Business Analyst, I decided to transition toward a career as a Software Engineer.
+            A recent graduate of Flatiron School’s Full-Time Software Engineering Bootcamp, I’ve completed over 45+ hours a week of study time and completed 4 group projects, developing full-stack applications, utilizing my knowledge of JavaScript, React, SQL, and Ruby. I’m a creative person at heart- ask me about improv comedy!- which is what led me to change my career from “facilitator of technology” to “creator of technology.”
+            </p>
+            <p class="lead">I strongly believe that my years of experience working as a Business Analyst enhanced with my new found skills in Programming would make me an incredible asset to any team.</p>
 
-A recent graduate of Flatiron School’s Full-Time Software Engineering Bootcamp, I’ve completed over 45+ hours a week of study time and completed 4 group projects, developing full-stack applications, utilizing my knowledge of JavaScript, React, SQL, and Ruby. 
+        
 
-I’m a creative person at heart- ask me about improv comedy!- which is what led me to change my career from “facilitator of technology” to “creator of technology.”
 
-I strongly believe that my years of experience working as a Business Analyst enhanced with my new found skills in Programming would make me an incredible asset to any team.</p>
                 <h2>Contact Info:</h2>
             <div class="container-sm">
                 <ul class="list-group">
