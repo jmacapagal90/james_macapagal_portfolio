1eea231c6a9a29c7246157359763c26d40541590
James Macapagal <j.macapagal@gmail.com>
2022-10-04 10:31:57 -0500

    changed hader size and position

--- docs/src/components/Home.js
+++ docs/src/components/Home.js
@@ -9,7 +9,8 @@ function Home(){
                             <div class="p-5 mb-4 bg-transparent text-bg-dark" >
                                 <div class="text-end">
                                     <div id="title-message">
-                                        <h1 class="display-6">James Macapagal <br></br>Software Engineer</h1>
+                                        <h1 class="display-5">James Macapagal</h1>
+                                        <h2 class="display-7">// software engineer</h2>
                                     </div>
                                 </div>
                                 
