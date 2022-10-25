fcb72e62a623f85c935f59b936ebbafe94e01d92
James Macapagal <j.macapagal@gmail.com>
2022-10-25 13:26:55 -0500

    finished updated Blog page

--- docs/src/components/BlogCard.js
+++ docs/src/components/BlogCard.js
@@ -1,3 +1,4 @@
+import { ThumbsupIcon, CommentIcon } from "@primer/octicons-react"
 
 
 function BlogCard({
@@ -24,13 +25,13 @@ function BlogCard({
                     <h6 class="card-subtitle mb-2 text-muted"></h6>
                     <p class="card-subtitle mb-2 text-muted">{reading_time} minute read</p>
                     <p class="card-text">{description}</p>
+
+                    <p class="card-text text-muted"><em>{tags.map((tag)=> `#${tag} `)}</em></p>
+                    <a class="btn btn-primary" target="_blank" href={url}>Read More</a>
                 </div>
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item text-muted">Likes: {likes}, {comments} comments</li>
-                    <li class="list-group-item text-muted"><em>{tags.map((tag)=> `#${tag}, `)}</em></li>
-                </ul>
-                <br></br>
-                <a class="btn btn-primary" target="_blank" href={url}>Read More</a>
+                <div class="card-footer text-muted">
+                    <p class="card-text"><ThumbsupIcon size={16}/> {likes} <CommentIcon size={16}/> {comments}</p>  
+                </div>
             </div>
         </div>
     )
